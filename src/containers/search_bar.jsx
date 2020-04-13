42319b63d42fd311ee348b132df9d16d777f3f80
Mehdi Hajikhani <mhaji007@fiu.edu>
2020-04-13 18:00:30 -0400

    Make input a controlled component

--- src/containers/search_bar.jsx
+++ src/containers/search_bar.jsx
@@ -1,13 +1,29 @@
 import React, {Component} from 'react'
 
 class SearchBar extends Component {
+
+    state = {
+        term: ''
+    };
+
+    onInputChange = (event) => { 
+        console.log(event.target.value);
+        this.setState({term:event.target.value})
+    }
+
     render() {
         return (
             <div>
                 <div className="ui container">
-                    <div class="ui action input">
-                        <input type="text" placeholder="Search..."/>
-                        <button class="ui button">Submit</button>
+                    <div className="ui action input">
+                        <input
+                            placeholder="Get a five-day forecast in your favorite cities"
+                            type="text"
+                            size="75"
+                            value={this.state.term}
+                            onChange={this.onInputChange}/>
+
+                        <button className="ui button">Submit</button>
                     </div>
 
                 </div>
